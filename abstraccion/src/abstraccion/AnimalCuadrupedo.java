995e66382874a04427358c6c0e4ef2253ced4732
LeandroPV701 <127234745+LeandroPV701@users.noreply.github.com>
2023-11-24 15:03:26 -0600

    Add files via upload

---
+++ abstraccion/src/abstraccion/AnimalCuadrupedo.java
@@ -0,0 +1,17 @@
+
+package abstraccion;
+
+/**
+ *
+ * @author Leandro
+ */
+public abstract class AnimalCuadrupedo {
+ 
+    public abstract void sonido();
+    
+    public abstract void VCarrera();
+    
+    public int CPatas(){
+        return 4;
+    }
+}
---
+++ abstraccion/src/abstraccion/gato.java
@@ -0,0 +1,17 @@
+
+package abstraccion;
+
+/**
+ *
+ * @author Leandro
+ */
+public class gato extends AnimalCuadrupedo {
+    @Override
+    public void sonido(){
+        System.out.println("Miau Miau");
+    }
+    @Override
+    public void VCarrera(){
+        System.out.println("Brr Brr");
+    }
+}

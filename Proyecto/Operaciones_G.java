8b946387f2e29ffbbd8eeb66372c227283b9ade4
LeandroPV701 <127234745+LeandroPV701@users.noreply.github.com>
2023-11-24 14:52:47 -0600

    Add files via upload

---
+++ Proyecto/Operaciones_G.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proyecto;
+
+/**
+ *
+ * @author Leandro
+ */
+public class Operaciones_G {
+    public double Poligono(double jTextPane3, double jTextPane4){
+        double res;
+        res=(jTextPane3*jTextPane4)/2;
+        return res;
+    }
+    
+    public double Rombo(double jTextPane5, double jTextPane6){
+        double res;
+        res=jTextPane5*jTextPane6;
+        return res;
+    }
+    public double Romboide(double jTextPane7, double jTextPane8, double jTextPane9){
+        double res;
+        res=(jTextPane9/(jTextPane8*jTextPane7))/2;
+        return res;
+    }
+    
+}
---
+++ Proyecto/Operaciones_e.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proyecto;
+
+/**
+ *
+ * @author Leandro
+ */
+public class Operaciones_e {
+    
+    public double Resistencia(double jTextPane4, double jTextPane3){
+        double res;
+        res=jTextPane4/jTextPane3;
+        return res;
+    }
+    
+    public double Voltaje(double jTextPane3, double jTextPane4){
+        double res;
+        res=jTextPane4*jTextPane3;
+        return res;
+    }
+    public double Potencia(double jTextPane3, double jTextPane4){
+        double res;
+        res=jTextPane4*jTextPane3;
+        return res;
+    }   
+    public double Corriente(double jTextPane3, double jTextPane1){
+        double res;
+        res=jTextPane3/jTextPane1;
+        return res;
+    }
+}
